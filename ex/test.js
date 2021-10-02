29b81a789197aec967ee37b8308761b4a9e38b1d
SamerLhiany <samer.l.htc@gmail.com>
2021-10-02 23:25:38 +0300

    update

---
+++ ex/test.js
@@ -0,0 +1,19 @@
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     id: 5566,
+//     fullName: function () {
+//         console.log (this.firstName + " " + this.lastName);
+//         console.log(person.firstName + " " + person.lastName);
+//     }
+// };
+
+// person.fullName ();
+
+// const person = {
+//     firstName: "mhmd",
+//     lastName: "abuahmd",
+//     id: 111};
+
+
+
